c0ac4fe08b20e2a290b76fed4ca9aaa13948ae25
sacOO7 <sachinshinde7676@gmail.com>
2022-05-23 23:55:42 +0530

    Added code to get a client token

--- client.js
+++ client.js
@@ -5,7 +5,18 @@ const Ably = require("ably");
 const ably = new Ably.Realtime(); // provide authUrl or authCallBack for receiving token
 
 /* Subscribe to the 'some_channel' channel with the Ably client */
-const ablyChannel = ably.channels.get(ablyChannelName);
-ablyChannel.subscribe(function(message) {
-  console.log('Ably client received a message: ' + message.name + ', data: ' + JSON.stringify(message.data));
-});
+// const ablyChannel = ably.channels.get(ablyChannelName);
+// ablyChannel.subscribe(function(message) {
+//   console.log('Ably client received a message: ' + message.name + ', data: ' + JSON.stringify(message.data));
+// });
+
+const main = async() => {
+  try {
+  const token = await ably.auth.requestToken();
+  console.log('token is ', token);
+  } catch(e) {
+    console.error(e)
+  }
+}
+
+main()
