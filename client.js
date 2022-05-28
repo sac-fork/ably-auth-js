62b6da58c9b87d8562ab5aeab7a29d3aa675cb12
sacOO7 <sachinshinde7676@gmail.com>
2022-05-28 11:13:59 +0530

    Updated client.js with explicit authorize

--- client.js
+++ client.js
@@ -1,32 +1,40 @@
 const Ably = require("ably");
 
-/* ABLY CLIENT LIB */
-/* Instance the Ably library */
-const ably = new Ably.Realtime(); // provide authUrl or authCallBack for receiving token
+// Creating a client with token based auth, way to renew token using API_KEY
+const ablyClient = new Ably.Realtime({
+  defaultTokenParams: {ttl: 20000, capability: '{"*":["*"]}'},
+  queryTime: true,
+  useTokenAuth : true,
+  key: '',
+  // log: {
+  //   "level": 3, // debug
+  //   "handler": (msg)=> {
+  //     console.log(msg);
+  //   }
+  // }
+});
 
-/* Subscribe to the 'some_channel' channel with the Ably client */
-// const ablyChannel = ably.channels.get(ablyChannelName);
-// ablyChannel.subscribe(function(message) {
-//   console.log('Ably client received a message: ' + message.name + ', data: ' + JSON.stringify(message.data));
-// });
+// listen to all events on connection
+ablyClient.connection.on((eventName, error)=> {
+  console.log("Connection event :: ", eventName, " error :: ", error);
+});
 
-ably.auth.requestToken(function(err, tokenDetails) {
-  // tokenDetails is instance of TokenDetails
-  // see https://www.ably.com/docs/rest/authentication/#token-details for its properties
+const ablyChannel = ablyClient.channels.get("channel1");
+ablyChannel.subscribe(function(message) {
+  console.log('channel1 message :: ' + message.name + ', data :: ' + JSON.stringify(message.data));
+}); 
+ablyChannel.on((eventName, error)=> {
+  console.log("channel1 event :: ", eventName, " error :: ", error);
+});
 
-  // Now we have the token, we can send it to someone who can instantiate a client with it:
-  console.error(err);
-  console.log(tokenDetails)
-  // var clientUsingToken = new Ably.Realtime(tokenDetails.token);
-})
+setTimeout(() => {
+  console.log("Explicitly authorizing with capability");
+  ablyClient.auth.authorize({ttl: 19000, capability: '{"*":["*"]}'});
+}, 2000);
 
-const main = async() => {
-  try {
-  const token = await ably.auth.requestToken();
-  console.log('token is ', token);
-  } catch(e) {
-    console.error(e)
-  }
-}
+// ably.auth.requestToken({clientId: '1234', ttl: 10000}, function(err, tokenDetails) {
+//   // tokenDetails is instance of TokenDetails
+//   // see https://www.ably.com/docs/rest/authentication/#token-details for its properties
 
-main()
+//   // Now we have the token, we can send it to someone who can instantiate a client with it:
+// })
